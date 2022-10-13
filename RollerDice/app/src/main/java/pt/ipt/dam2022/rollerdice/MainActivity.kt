b647973626b6b61d24b1cc58ea2493839fbb1677
jcnpereira <casimiro@ipt.pt>
2022-10-14 00:02:29 +0100

    add of ImageView to present dice

--- RollerDice/app/src/main/java/pt/ipt/dam2022/rollerdice/MainActivity.kt
+++ RollerDice/app/src/main/java/pt/ipt/dam2022/rollerdice/MainActivity.kt
@@ -3,6 +3,7 @@ package pt.ipt.dam2022.rollerdice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -34,6 +35,19 @@ class MainActivity : AppCompatActivity() {
         var resultText = findViewById<TextView>(R.id.resultTextView)
         // change the content of TextView
         resultText.text = randomDiceValue.toString()
-     
+
+        // define the face of dice to show on screen
+        var drawableResource = when (randomDiceValue) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        // assign the image to the ImageView
+        var diceImage = findViewById<ImageView>(R.id.diceImageView)
+        diceImage.setImageResource(drawableResource)
     }
 }
